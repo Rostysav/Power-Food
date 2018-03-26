723f050aaf0ddc7ba85c3b83224fe686bc30d604
Rostyslav <rostyslav4yk@gmail.com>
2018-03-26 11:31:46 +0300

    header and main section

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -10,6 +10,19 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  	 window.addEventListener('scroll', function() {
+      // //Here you forgot to update the value
+      const scrollPos = window.scrollY;
+      const header = document.getElementById('header');
+
+      if (scrollPos > 100) {
+        header.classList.add('black-bg');
+      } else {
+        header.classList.remove('black-bg');
+      }
+    });
+
   }
 
 }
