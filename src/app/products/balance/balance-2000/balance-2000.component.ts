a7fb5717f066e5462afb57f08ae4d044eec46a05
Rostyslav <rostyslav4yk@gmail.com>
2018-06-05 11:32:45 +0300

    some-bugs

--- src/app/products/balance/balance-2000/balance-2000.component.ts
+++ src/app/products/balance/balance-2000/balance-2000.component.ts
@@ -18,7 +18,7 @@ export class Balance2000Component implements OnInit {
       { id: 2000,
         name:"Баланс - 2000ккал",
         price: 1203,
-        imageUrl: "https://levelkitchen.com/upload/iblock/e6c/e6c770196583e33b3826801628f61bce.jpg"}
+        imageUrl: "../../../assets/img/balance-2000.jpg"}
     ];
     localStorage.setItem('product', JSON.stringify(myArray));
   }
@@ -27,12 +27,10 @@ export class Balance2000Component implements OnInit {
     {
       'id': 2000,
       'name': 'Баланс - 2000ккал',
-      'description': 'Високий вміст білків з підтриманням оптимального рівня жирів і повільних вуглеводів' +
-      ' допоможе позбутися від зайвих кілограмів без втрати м\'язової маси.' +
-      ' Раціон - 2000 ккал.',
+      'description': 'Рівномірне співвідношення білків і вуглеводів та оптимальна кількість жирів сприяють підтримці комфортної ваги. Продуманий раціон дозволяє отримати близько 2000 ккал на добу.',
       'price': 1203,
       'dayPrice': 200,
-      'imageUrl': 'https://levelkitchen.com/upload/iblock/e6c/e6c770196583e33b3826801628f61bce.jpg'
+      'imageUrl': '../../../assets/img/balance-2000.jpg'
     }
   ];
 
