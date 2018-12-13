21a7b14a6fb285625759bd86848dd076105c2265
Volodymyr <z_valdemar@ukr.net>
2018-12-13 22:46:20 +0200

    add toastr with check working hours

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {ViewEncapsulation} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { ToastService } from "../../service/toastr.service";
 
 @Component({
   selector: 'pf-main',
@@ -9,9 +10,22 @@ import {ViewEncapsulation} from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastService: ToastService) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.checkIfWorkingHours();
+    }, 0)
+  }
+
+  checkIfWorkingHours() {
+    let currentTime = new Date().getHours();
+    if (currentTime >= 22 || currentTime <= 8) {
+      this.toastService.showToast(
+        'warning',
+        `Вибачте, зараз доставка не працює! Ви можете замовити продукти, менеджер зв'яжеться з вами в робочий час!`,
+        5000);
+    }
   }
 
 }
